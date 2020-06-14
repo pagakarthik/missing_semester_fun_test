5dbf9e6cef3bdbf35bf212396ea2dadf7d0a7439
Karthik Paga <kpaga@smart-ag.com>
2020-06-14 12:48:34 -0500

    test questions and masked instructions

---
+++ README.md
@@ -0,0 +1,4 @@
+Every file except test_3.py needs to be opened using a file editor and answered in line. 
+test_3.py is a program from one of the lessons, that you will be executing in one of the questions. I can't quite remember which one, but you will have fun.
+
+If things get out of order, hit Ctrl-C or Ctrl-\
---
+++ test_4.py
@@ -0,0 +1,8 @@
+If you are seein the contents here, then you learned how to use vim, git and a lot more!
+Very few people can say that! 
+
+What is your super power? "Do Su"
+No longer a test, few comments:
+
+    1. My favorite git commands are "clone"; "checkout"; "pull"; "push"; "branch"; "log".
+    Do you agree? 
